55a57204ea0d9c5055f7a6af8a0dec1fd6f6920d
HYrecovery <mcfengye1003@outlook.com>
2026-03-11 11:14:48 +0800

    Completed GenerateText Api, for personal use.

--- HuanyuAPI/Controllers/GenerateController.cs
+++ HuanyuAPI/Controllers/GenerateController.cs
@@ -70,5 +70,70 @@ namespace HuanyuAPI.Controllers
                 }));
             return Redirect(imageUrl);
         }
+
+        [HttpGet]
+        public IActionResult RoddyGenText(string key,string prompts)
+        {
+            string route = "GenerateController.RoddyGenText";
+            string ip = Common.GetClientIp(HttpContext);
+
+            string api = "https://ark.cn-beijing.volces.com/api/v3/chat/completions";
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {key}");
+
+            var payload = new Dictionary<string, object>
+            {
+                ["model"] = "doubao-seed-2-0-lite-260215",
+                ["messages"] = new[]
+                {
+                    new Dictionary<string, object>
+                    {
+                        ["role"] = "user",
+                        ["content"] = prompts
+                    }
+                },
+                ["thinking"] = new Dictionary<string, object>
+                {
+                    ["type"] = "disabled"
+                }
+            };
+
+            var response = client.PostAsJsonAsync(
+                api,
+                payload
+            );
+            string result = response.Result.Content.ReadAsStringAsync().Result;
+            string genText = "";
+            try
+            {
+                var doc = JsonDocument.Parse(result);
+
+                genText = doc!
+                    .RootElement!
+                    .GetProperty("choices")[0]!
+                    .GetProperty("message")!
+                    .GetProperty("content")!
+                    .GetString()!;
+            }
+            catch (Exception ex)
+            {
+                Log.SaveLog(l.FetchString("logActionWithException", new Dictionary<string, string>
+                {
+                    { "{user}", ip },
+                    { "{name}",  route + "/" + key + "&" + prompts },
+                    { "{exception}", ex.ToString() },
+                }));
+                return BadRequest($"Origin Json result : {result}\n" +
+                    $"Exception: " + ex);
+            }
+            Log.SaveLog(l.FetchString("logActionWithResult", new Dictionary<string, string>
+                {
+                    { "{user}", ip },
+                    { "{name}",  route + "/" + prompts },
+                    { "{result}", genText },
+                }));
+            return Ok(genText);
+        }
     }
 }
